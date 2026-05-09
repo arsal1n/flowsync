a1c5fef5b44c077c9a0c7f8fe9c96c5c34045d19
arsal1n <clashdoctor77@gmail.com>
2026-05-09 15:22:43 +0400

    Add safe maps routing workspace test page

---
+++ frontend/src/mapsWorkspaceMain.jsx
@@ -0,0 +1,13 @@
+// frontend/src/mapsWorkspaceMain.jsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./App.css";
+import MapsRoutingWorkspace from "./pages/MapsRoutingWorkspace";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MapsRoutingWorkspace />
+  </React.StrictMode>
+);
